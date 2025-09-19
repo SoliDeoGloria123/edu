b008c648a4bdf393b6b589b10c92d0d171cfef77
juan_david <juan.juand.dev@gmail.com>
2025-09-18 23:57:40 -0500

    feat(backend): add CORS middleware for handling cross-origin requests

--- backend/mainBackend.py
+++ backend/mainBackend.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from backend.Controller.userController import app as user_app
 
 app = FastAPI()
-
 # Montar las rutas de usuarios
 app.include_router(user_app.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 print("Backend esta ready")
 
